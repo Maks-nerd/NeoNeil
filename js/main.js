ea0705b63ff278ad63ac352ea69fcceb6ea8d528
Максим Калишенко <69075692+Maks-nerd@users.noreply.github.com>
2020-08-02 14:17:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(window).ready(function() {
+
+  $('.flexslider').flexslider({
+  	 selector: ".sliders > .slider",
+    animation: "slide",
+        minItems: 1,                 
+    maxItems: 1,                 
+    move: 1, 
+  });
+  
+});
